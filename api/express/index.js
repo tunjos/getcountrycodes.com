7bde0692b11c4a67fc80292a3b49301490adac1f
Tunji Olu-Taiwo <tunjos@tunjos.co>
2019-05-10 05:30:50 +0200

    Implement /countries API
    - Add sample countries.json
    - npm: install load-json-file

--- api/express/index.js
+++ api/express/index.js
@@ -1,17 +1,53 @@
 const express = require('express')
+const loadJsonFile = require('load-json-file');
+const url = require('url')
+const path = require('path');
+
 const app = express()
 
-app.get('/card', (req, res) => res.send('Hello Card!'))
+app.set('json spaces', 2);
 
 app.get('*', (req, res) => {
-  console.log("Hello World!");
-  res.json({ hello: "world" });
+  console.log(`Current directory: ${process.cwd()}`);
+  console.log("Url: " + req.url);
+
+  var country = req.url.split('/')[4]
+  var country = decodeURIComponent(country)
+  console.log("Country: " + country);
+
+  var detail = req.url.split('/')[5]
+  console.log("Detail: " + detail);
+
+  var countriesJson = path.join(__dirname, '../countries.json')
+  var countries = loadJsonFile.sync(countriesJson)
+
+  if (country == '' || country == 'undefined') {
+    res.json(countries)
+  } else if (countries.hasOwnProperty(country)) {
+    countryJson = countries[country]
+    if (countryJson.hasOwnProperty(detail)) {
+      res.json({
+        detail: countryJson[detail]
+      })
+    } else if (detail == '' || detail == 'undefined') {
+      res.json(countries[country])
+    } else {
+      res.status(400).json({
+        error: 'Invalid Country/Detail specified'
+      })
+    }
+  } else {
+    res.status(400).json({
+      error: 'Invalid Country specified'
+    })
+  }
 })
 
 app.all('*', (req, res) => {
   console.log("405");
-  res.status(405).json({ error: 'only POST requests are accepted' })
+  res.status(405).json({
+    error: 'only POST requests are accepted'
+  })
 })
 
-// app.listen()
 module.exports = app
