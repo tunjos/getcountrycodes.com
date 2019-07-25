739a7ab99814217dede54558ef2093ce02385ea1
Tunji Olu-Taiwo <tunjos@tunjos.co>
2019-07-25 15:31:02 +0200

    build.sh: Modify build

--- www/public/docs/build.sh
+++ www/public/docs/build.sh
@@ -1,13 +1,9 @@
 yum install ruby24.x86_64 ruby23-devel.x86_64
 
-gem uninstall ffi
-
-gem install ffi --platform=ruby
-
 gem install jekyll
 
-gem install bundler --version=2.0.1
+gem install bundler --no-ri --no-rdoc --version=2.0.1
 
 bundle install
 
-bundle exec jekyll build
+jekyll build
